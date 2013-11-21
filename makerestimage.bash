19d0e628e30c333a5d503be07192e3faa9ead07d
Will Foran <will.foran@gmail.com>
2013-11-21 11:27:57 -0500

    rename and reorganize, log script version

--- makerestimage.bash
+++ makerestimage.bash
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 #
-# check for anatomical image (fail out if DNE)
 # check for epi (Dimon from dcm if DNE)
+# dcm2nii does not orient some brains correctly 
+# output should be afni format for intput to afni_restproc.py
 # 
 
 # this expects to be sourced by another function
@@ -36,23 +37,24 @@ if [ ! -r $origepi ]; then
  cd $restdir
  Dimon -infile_prefix $restdcmdir/ \
 	    -GERT_Reco \
-	    -quit \
 	    -dicom_org \
 	    -sort_by_acq_time \
-	    -gert_write_as_nifti \
 	    -gert_create_dataset \
-	    -gert_to3d_prefix Dimon_$(basename $origepi)
+	    -gert_to3d_prefix $(basename ${origepi%%+orig.HEAD*}) \
+	    -quit 
+	    #-gert_write_as_nifti \
+ cd -
  #mv $(basename $origepi)* $(dirname $origepi)/Dimon_$(basename $origepi)
 
- # mricron tool: nii yes, gz yes,  no reorient, no crop, ,annonymize, no protocol in named,no date in name, output to folder
- dcm2niigz=$(dcm2nii -n y -g y  -r n -x n -a y -p n -d n -o $(dirname $origepi) ${restdcmdir}/* | sed -n 's/GZip...//p' )
- [ -z "$dcm2niigz" -o ! -r "$dcm2niigz" ] && echo "$dcm2niigz:dcm2nii failed? did not create $origepi" && exit 1
- 3dcopy $dcm2niigz $origepi
- cd -
+ # # mricron tool: nii yes, gz yes,  no reorient, no crop, ,annonymize, no protocol in named,no date in name, output to folder
+ ## not used because some are dcm->nii's appear flipped!
+ # dcm2niigz=$(dcm2nii -n y -g y  -r n -x n -a y -p n -d n -o $(dirname $origepi) ${restdcmdir}/* | sed -n 's/GZip...//p' )
+ # [ -z "$dcm2niigz" -o ! -r "$dcm2niigz" ] && echo "$dcm2niigz:dcm2nii failed? did not create $origepi" && exit 1
+ # 3dcopy $dcm2niigz $origepi
 fi
 
 #### anatomical check
 # not the best place, but it's a place :)
-[ ! -r $origmprage ] && echo "no mprage!!! run qsub mprage_to_MNI.bash -Vv SUBJECT=$SUBJECT,VISIT=$VISIT" && exit 1
+#[ ! -r $origmprage ] && echo "no mprage!!! run qsub mprage_to_MNI.bash -Vv SUBJECT=$SUBJECT,VISIT=$VISIT" && exit 1
 
-echo "rest image is read"
+#echo "rest image is read"
--- onlyafniproc.bash
+++ onlyafniproc.bash
@@ -1,18 +1,35 @@
 #!/usr/bin/env bash
-set -xe
-
-# three cpus for 5 hours
-# afni will eat all cpus for some bits
+# PBS SETTINGS:
+#   three cpus for 5 hours
+#   afni will eat all cpus for only some bits of streem
 #PBS -l ncpus=3
 #PBS -l walltime=5:00:00
-#dont use leading zeros
 #PBS -q batch
+#
+#
+#2013-11-21 WF
+# setup for afni_preproc for power neuroimage 2012 (no global signal regression)
+# copy of script validated by SM on skynet:/Volumes/Phillips/Rest/rest_scripts/rest_preproc_transition
+
+# get script info -- log to file
+scriptdir=$(cd $(dirname $0);pwd)              
+# if torque has copied the script, we need the hardcoded path
+[[ $scriptdir =~ spool ]] && scriptdir="/home/foranw/src/restPreproc"
+scriptdateversion="$(perl -slane 'if(m/\d{4}-\d{2}-d{2}/){print $&}' $scriptdir/$(basename $0))"
+scriptgitversion="$(cd $scriptdir; git log|sed 1d)"
+
+set -xe
+
 
 # ./onlyafniproc.bash -sid 10152_20100514 -sdir /Volumes/Phillips/Rest_Reward/10152_20100514/ -aseg /Volumes/Phillips/Rest_Reward/10152_20100514/anat/aseg.mgz -t1 /Volumes/Phillips/Rest_Reward/10152_20100514/anat/T1.mgz -t2 /Volumes/Phillips/Rest_Reward/10152_20100514/rest/all.nii.gz -physio /Volumes/Phillips/Rest/physio1D/10152_RetroTS.slibase.1D
 ### OR
 # qsub onlyafniproc.bash -vV sid="10152_20100514",sdir="/data/Luna1/Reward/Rest/10152_20100514/",aseg="/data/Luna1/Reward/FS_Subjects/10152_20100514/mri/aseg.mgz",t1="/data/Luna1/Reward/FS_Subjects/10152_20100514/mri/T1.mgz",t2="/data/Luna1/Reward/Rest/10152_20100514/Dimon_rest.nii.gz.nii.gz",physio="/data/Luna1/Reward/Physio/10152/20100514/rest_21_RetroTS.slibase.1D"
 
-TR=1.5
+## Where to save output
+# e.g. /data/Luna1/Reward/Rest/10152_20100514/afni_restproc/power_nogsr
+afnidirname=afni_restproc
+innerdir=power_nogsr
+
 while [ -n "$1" ]; do
  case $1 in 
   -sdir)      sdir=$2;        shift 2;;  # subject's folder -- where to save mprage/ and rest/
@@ -20,8 +37,7 @@ while [ -n "$1" ]; do
   -t1)        t1=$2;          shift 2;;  # t1 image, mgz format (/Volumes/Serena/Rest/FS_Subjects/${sid}/mri/T1.mgz)
   -aseg)      aseg=$2;        shift 2;;  # FS segmentation, mgz format (/Volumes/Serena/Rest/FS_Subjects/${sid}/mri/aseg.mgz) 
   -t2)        t2=$2;          shift 2;;  # functional image, nii format
-  -TR)        TR=$2;          shift 2;;  # TR of resting state scan (default 2)
-  -physio)    physio=$2;      shift 2;;  # TR of resting state scan (default 2)
+  -physio)    physio=$2;      shift 2;;  # location of physio file
   *) echo -e "[Unrecognized option '$1']"; 
      sed -ne "s:\$0:$0:g;s/# //p;/END/q" $0;                             # print header
      perl -lne 'print "\t$1:\t$2" if m/^\s+(-.*)\).*shift.*# (.*)$/' $0; # print options
@@ -42,54 +58,30 @@ for varname in sid sdir t1 t2 physio; do
   # don't need to be able to read sdir, we will make it
   [ "$varname" = "sdir" ] && continue
 
-  # sanity check for file existance. should never hit this
+  # sanity check for file existance
   [ ! -r ${!varname} ] && echo "could not read $varname (${!varname}), exiting" && exit 1;
 done
 
-afnidirname=afni_rest_preproc
-[  -r $sdir/$afnidirname  ] && rm -r $sdir/$afnidirname
-mkdir -p $sdir/$afnidirname 
+
+[  -r $sdir/$afnidirname/$innerdir && -n "$REDO" ] && rm -r $sdir/$afnidirname/$innerdir
+[  -r $sdir/$afnidirname/$innerdir ] && echo "ALREADY RUN!" && exit 1
+
+mkdir -p $sdir/$afnidirname
 cd $sdir/$afnidirname
-# covnert mprage from FS
-#mri_convert $t1 mprage+orig.HEAD
 
-#nifti to AFNI
-#3dcopy $t2 restepi+orig
+## TODO: pull sql info
 
-# dcm2nii is off
-#Dimon -infile_prefix $(dirname $t2)/ -GERT_Reco -dicom_org -sort_by_acq_time -gert_create_dataset -gert_to3d_prefix ${sid}_restepi -quit
+## write to processing log (script,time,version,git version)
+echo -e "$(date +%F\|%H:%M)\tstart\t$scriptdir/$(basename $0) ($scriptdateversion/$scriptgitversion)" >> $sdir/processing.log
 
-# t1 and aseg can be .mgz
-# if restepi is nii.gz, renaming gets funky
-afni_restproc.py \
-  -anat $t1     \
-  -epi  $t2   \
-  -aseg $aseg          \
-  -rvt  $physio        \
-  -prefix restpp \
-  -dest   restpp  \
-  -script preproc_$sid.tcsh \
-  -anat_has_skull yes \
-  -smoothrad 4 \
-  -venterode 2 \
-  -wmerode 1   \
-  -bandpass \
-  -setbands 0.009 0.08 \
-  -bpassregs \
-  -dreg \
-  -tlrclast \
-  -exec  off \
-  -tsnr 2>&1  #| tee afni_$sid.log
-  
-
-
+#### ACTUALL RUN
 afni_restproc.py \
 	-despike off \
 	-aseg $aseg \
 	-anat $t1 \
 	-epi  $t2 \
-	-script power_method.tcsh \
-	-dest restpreproc_power \
+	-script power_nogsr.tcsh \
+	-dest power_nogsr \
 	-prefix pm \
 	-tlrc \
 	-dvarscensor \
@@ -106,7 +98,48 @@ afni_restproc.py \
 	-censorright 2 \
 	-fdlimit 0.5 \
 	-dvarslimit 5 \
-	-modenorm  | tee afniPower_$sid.log
+	-modenorm  | tee power_nogsr_$sid.log
+
+
+# all went well
+echo -e "$(date +%F\|%H:%M)\tfinish\t$scriptdir/$(basename $0) ($scriptdateversion/$scriptgitversion)" >> $sdir/processing.log
+exit 0
+
+### LEFTOVERs
+
+# covnert mprage from FS
+#mri_convert $t1 mprage+orig.HEAD
+
+#nifti to AFNI
+#3dcopy $t2 restepi+orig
+
+# dcm2nii is off
+#Dimon -infile_prefix $(dirname $t2)/ -GERT_Reco -dicom_org -sort_by_acq_time -gert_create_dataset -gert_to3d_prefix ${sid}_restepi -quit
+
+# t1 and aseg can be .mgz
+# if restepi is nii.gz, renaming gets funky
+#afni_restproc.py \
+#  -anat $t1     \
+#  -epi  $t2   \
+#  -aseg $aseg          \
+#  -rvt  $physio        \
+#  -prefix restpp \
+#  -dest   restpp  \
+#  -script preproc_$sid.tcsh \
+#  -anat_has_skull yes \
+#  -smoothrad 4 \
+#  -venterode 2 \
+#  -wmerode 1   \
+#  -bandpass \
+#  -setbands 0.009 0.08 \
+#  -bpassregs \
+#  -dreg \
+#  -tlrclast \
+#  -exec  off \
+#  -tsnr 2>&1  #| tee afni_$sid.log
+#  
+#
+#
         #-exec  off \
 
 #tcsh -xef preproc_$sid.tcsh 2>&1 | tee preprocout.$sid.log
